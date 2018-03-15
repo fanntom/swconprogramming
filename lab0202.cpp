cb588cfc71049829457ee2cc8e10aa145be50b31
Park Seong Wook(Brian Park) <imbrianp@khu.ac.kr>
2018-03-15 11:36:01 +0900

    Create lab0202.cpp

---
+++ lab0202.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A;
+	cout << "Please enter A: ";
+	cin >> A;
+	int B;
+	cout << "Please enter B: ";
+	cin >> B;
+	cout << "A+B is " << A+B << ", A-B is " << A-B << ", A*B is " << A*B << ", A/B is " << A/B << ", and A%B is " << A%B;
+}
